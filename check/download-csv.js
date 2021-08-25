2cc2501972d3f0963fa7f29b855e6d809217790b
zummy <tech.zummy@gmail.com>
2021-08-25 20:19:15 +0900

    rename files.

--- check/download-csv.js
+++ check/download-csv.js
@@ -7,14 +7,32 @@ function button1_clicked(evt) {
   evt.preventDefault();
 
   const lines_csv = [
-    ["this, is, CSV, file, sample", "\n"],
-    ["this, is, CSV, file, sample", "\n"]
+    [
+      'IHP-2105-001',
+      1,
+      '4526112285341',
+      'PW-2',
+      40,
+      'A​-47-1-2',
+      '2021-05-01 09:01:02',
+      "\n"
+    ],
+    [
+      'IHP-2105-001',
+      2,
+      '4526112286683',
+      'PW-3',
+      25,
+      'K-7-85-3',
+      '2021-05-01 09:12:13',
+      "\n"
+    ]
   ];
   const blob = new Blob(lines_csv, {type: 'text/plain'});
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
   document.body.appendChild(a);
-  a.download = 'blob_sample_csv.txt';
+  a.download = 'ihr-csv.txt';
   a.href = url;
   a.click();
   a.remove();
